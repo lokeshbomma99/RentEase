b0c060d6e436ea4229457e81356e8dc872a50c92
lokeshbomma99 <169030699+lokeshbomma99@users.noreply.github.com>
2026-03-04 23:33:31 +0530

    Change API proxy target to Vercel URL

--- client/vite.config.js
+++ client/vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5003',
+        target: 'https://rent-ease-red.vercel.app/',
         changeOrigin: true,
       }
     }
